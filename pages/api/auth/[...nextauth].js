ee430e4d8db16677e1754424699b2ff6e8b47c5b
Ratatouille <cshimmaincontact@gmail.com>
2023-11-06 17:42:43 -0500

    admin security

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,12 @@
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-export default NextAuth({
+const adminEmails = ["cshimmaincontact@gmail.com"]
+
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -13,4 +15,25 @@ export default NextAuth({
   ],
 
   adapter: MongoDBAdapter(clientPromise),
-});
+  callbacks: {
+    session: ({ session, token, user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw "Not an Admin";
+  }
+}
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,8 +3,12 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import fs from "fs";
 import mime from "mime-types";
 const bucketName = "nextjs-soda";
+import { isAdminRequest } from "./auth/[...nextauth]";
 
 export default async function handle(req, res) {
+  await mongooseConnect();
+  await isAdminRequest(req,res)
+
   const form = new multiparty.Form();
   const { fields, files } = await new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
